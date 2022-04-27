d3826a9e944430e391de974814c8c748c30ee18e
Gabriel <90275961+GarielFer@users.noreply.github.com>
2022-04-27 13:50:28 -0300

    Update q8.c

--- Roteiro02/q8.c
+++ Roteiro02/q8.c
@@ -1,41 +1,53 @@
-//Editar o nome de um contato a partir de um indentificador (ID)
+//Editar um contato a partir de um indentificador (ID)
 #include<stdio.h>
 
-void editar(int idA);
-
+void editar(int idA); //Definir uma função
 int main(){
-    int a;
+int a; //criei uma variavel para receber o id que o usuario deseja editar
     printf("Informe o id do contato que deseja editar: ");
     scanf("%i",&a);
-    editar(a);
+
+    editar(a); //Chamei a função usando essa variavel
 }
 
 void editar(int idA){
-    FILE *f1 = fopen("agenda_aux.dat", "w");
-    FILE *f2 = fopen("agenda.dat", "r");
+    //Criei dois ponteiros para abrir os dois arquivos
+    FILE *f1 = fopen("agenda_aux.dat", "w"); //esse arquivo é para escrever
+    FILE *f2 = fopen("agenda.dat", "r"); // esse arquivo é para ler as informações da agenda
 
+    //Essas são as variaveis que vão receber as informações das agendas/arquivos
     int id;
     char nome[80];
     char profissao[80];
     char telefone[80];
 
-    if(!f2){
+    if (!f1){ //Se o arquivo que o ponteiro está apontando não abra, essa vai ser a mensagem que vai aparecer
         printf("Impossivel abrir o arquivo\n");
     }
 
-    while(!feof(f2)){
+    if(!f2){  //Se o arquivo que o ponteiro está apontando não abra, essa vai ser a mensagem que vai aparecer
+        printf("Impossivel abrir o arquivo\n");
+    }
+    
+    while(!feof(f2)){ //Criei um laço de repetição que só vai parar quando chegar ao fim do arquivo
+        //Esse fscanf vai ler todos os elementos que estiverem no arquivo em que o ponteiro está apontando, no caso o agenda.dat
         fscanf(f2, "%d %s %s %s",&id, nome, profissao, telefone);
-            if(id != idA){
-                fprintf(f1,"%d %s %s %s\n",id, nome, profissao,telefone);
-            }else if(id == idA){
-                printf("(%d) (%s) (%s)\n",id, nome, telefone);
-                printf("Insira um novo nome para esse id: ");
-                scanf("%s",nome);
-                fprintf(f1,"%d %s %s %s\n",id, nome, profissao,telefone); 
-                printf("(%d) (%s) (%s)\n",id, nome, telefone);   
-            }       
+
+        //Coloquei esse printf para mostrar ao usuario todos os nomes
+        printf("(%d) (%s) (%s)\n",id, nome, telefone);
+    
+        /*Enquanto o id for diferente do que o usuario pediu esse fprintf vai ficar colando os nomes no arquivo em que o
+        ponteiro está apontando, nesse caso o arquivo agenda_aux.dat*/
+        if(id != idA){
+            fprintf(f1,"%d %s %s %s\n",id, nome, profissao,telefone);
+
+        }else if(id == idA){ //Aqui vai parar no que o usuário informou e vai pedir um novo nome para aquele id
+            printf("Insira um novo nome para esse id: ");
+            scanf("%s",nome);
+            //Com isso  coloquei outro fprintf para colocar la no arquivo o nome que o usuario modificou desse id
+            fprintf(f1,"%d %s %s %s\n",id, nome, profissao,telefone);    
+        }       
     }
     fclose(f1);
     fclose(f2);
-
 }
